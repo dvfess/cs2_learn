a24498495fb8690121b208f8eb551227fe53737e
Volkov Dmitriy <mcslash@yandex.ru>
2019-05-24 13:55:30 +0300

    init

---
+++ Departments/Employees.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Departments
+{
+    class Employees
+    {
+        public string Name { get; set; }
+        public int DepID { get; set; }
+    }
+
+    class Department
+    {
+        public int DepID { get; set; }
+        public string Name { get; set; }
+    }
+}
